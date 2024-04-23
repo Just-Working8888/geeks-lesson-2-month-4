7341fc26bcf5f8317d0286e68a70907acb1045f7
Bekbolot <justworking8888@gmail.com>
2024-04-23 19:21:55 +0600

    fsdsf

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Header from './components/Header'
+import Footer from './components/Foooter';
+import Card from './components/Card';
+import { useState } from 'react';
 function App() {
+  const [state, setState] = useState('green')
+  function handleColorChange(color) {
+    setState(color)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {state}
+
+      <div style={{ display: 'flex' }}>
+        <div style={{ background: 'red', padding: '2rem' }}>
+          <button onClick={() => handleColorChange('red')}>red</button>
+        </div>
+        <div style={{ background: 'blue', padding: '2rem' }}>
+          <button onClick={() => handleColorChange('blue')}>blue</button>
+        </div>
+        <div style={{ background: 'yellow', padding: '2rem' }}>
+
+          <button onClick={() => handleColorChange('yellow')}>yellow</button></div>
+        <div style={{ background: 'green', padding: '2rem' }}>
+
+          <button onClick={() => handleColorChange('green')}>green</button>
+        </div>
+      </div>
+
+      <div style={{ background: state, padding: '2rem' }}>
+      </div>
+      <div className='container'>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <Footer />
+
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+function Header() {
+
+    return (
+        <header>
+            <h1>Logo</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Home</li>
+
+                    <li>Home</li>
+
+                    <li>Home</li>
+
+                </ul>
+            </nav>
+            <button>Cart</button>
+        </header>
+    )
+}
+export default Header
